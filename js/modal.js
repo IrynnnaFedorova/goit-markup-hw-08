69e4c5657e309afa2d7fef1ce3cd4e5ef0d2c0eb
IrynnnaFedorova <bulhakovairynnna@gmail.com>
2021-11-04 19:27:31 +0200

    Добавляет правки ментора с меню

--- js/modal.js
+++ js/modal.js
@@ -12,10 +12,3 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
-
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  menuBtnRef.addEventListener("click", () => {
-    menuBtnRef.classList.toggle("is-open");
-  });
-})();
